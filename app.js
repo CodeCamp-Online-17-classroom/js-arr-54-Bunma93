77ef8ed172c25d41f8bade52ba0bd9e5f1b0cf15
Bunma93 <bunma93@gmail.com>
2024-07-27 06:33:10 +0700

    Js-arr54

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+let names = ['Jack', 'John', 'Bill', 'John', 'Rick', 'Bill', 'John'];
+
+let nameCount = names.reduce((acc, name) => {
+    acc[name] = (acc[name] || 0) + 1;
+    return acc;
+}, {});
+
+console.log(nameCount);
